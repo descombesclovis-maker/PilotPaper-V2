89fa208357aae4794d490e27040be0039c29943f
descombesclovis-maker <descombesclovis@gmail.com>
2026-09-14 17:59:57 +0200

    Remove Site Twin bridge from PilotPaper V1 page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { DpPieceWorkbenchClient } from "@/components/dp-piece-workbench-client";
 import { PilotPaperUpdateButton } from "@/components/pilotpaper-update-button";
-import { SiteTwinAddressBridge } from "@/components/site-twin-address-bridge";
 
 export default function Page() {
   return (
     <>
-      <SiteTwinAddressBridge />
       <DpPieceWorkbenchClient />
       <PilotPaperUpdateButton />
     </>
